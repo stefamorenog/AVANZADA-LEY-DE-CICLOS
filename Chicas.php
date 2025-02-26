ec24a5ae81943f5a871e0ae2bb03f6980067acad
stefamorenog <moreno.stefania17@gmail.com>
2025-02-26 00:52:16 -0500

    Cambios en estilos

--- Chicas.php
+++ Chicas.php
@@ -28,6 +28,8 @@ if (!empty($estado_filtrado)) {
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
     <title>Lista de Chicas Mágicas</title>
     <style>
         body {
@@ -82,9 +84,9 @@ if (!empty($estado_filtrado)) {
         }
     </script>
 </head>
-<body>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-     <nav class="navbar navbar-expand-lg bg-warning">
+<body> 
+<div class="container-fluid">
+  <nav class="navbar navbar-expand-lg bg-warning">
         <div class="container-fluid">
         <a class="navbar-brand  text-white" href="Index.php"><h2>LEY DE LOS CICLOS</h2></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -101,7 +103,7 @@ if (!empty($estado_filtrado)) {
       </ul>
     </div>
     </div>
-        </nav>
+  </nav>
 
     <h1 class="p-4 justify-content-center">Chicas Mágicas Registradas</h1>
 
@@ -159,8 +161,8 @@ if (!empty($estado_filtrado)) {
     </table>
 
     <!-- BOTÓN PARA VER HISTORIAL -->
-    <a href="historial.php"><button class="historial">Ver Historial de Cambios</button></a>
-    <a href="index.php"><button>Regresar</button></a>
-
+    <a href="historial.php" class="p-4 justify-content-center"><button class="historial">Ver Historial de Cambios</button></a>
+    <a href="index.php" class="p-4 justify-content-center"><button>Regresar</button></a>
+          </div>
 </body>
 </html>
